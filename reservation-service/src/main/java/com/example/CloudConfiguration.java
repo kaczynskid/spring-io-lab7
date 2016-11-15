ccc182ee6093df0dd83bc82939caec22c73f008c
Darek Kaczyński <kaczynskid@gmail.com>
2016-11-15 11:12:57 +0100

    Added registry client to reservation-service

---
+++ reservation-service/src/main/java/com/example/CloudConfiguration.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("cloud")
+@Configuration
+@EnableDiscoveryClient
+public class CloudConfiguration {
+}
