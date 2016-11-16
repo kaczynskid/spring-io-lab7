13c54b366ad829ce8928d959ab08868adecdaec2
Darek Kaczyński <kaczynskid@gmail.com>
2016-11-16 11:13:03 +0100

    Implemented verifier based on contract

--- verifier-service/src/test/java/com/example/VerifierServiceApplicationTests.java
+++ verifier-service/src/test/java/com/example/VerifierServiceApplicationTests.java
@@ -1,16 +1,30 @@
 package com.example;
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = VerifierServiceApplication.class)
 public class VerifierServiceApplicationTests {
+
+	@Autowired
+    WebApplicationContext applicationContext;
+
+    @Before
+    public void setup() {
+        RestAssuredMockMvc.webAppContextSetup(applicationContext);
+    }
 
 	@Test
 	public void contextLoads() {
 	}
-
 }
