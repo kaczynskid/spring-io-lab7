906ee5f63065a202c0975b735de803c1379c225d
Darek Kaczyński <kaczynskid@gmail.com>
2016-11-16 11:33:05 +0100

    Changed ELK config to use only directory mounts

--- elk.sh
+++ elk.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 mkdir -p /tmp/elk-data
-cp -u ./logstash-input.conf /tmp/logstash-input.conf
-cp -u ./logstash-output.conf /tmp/logstash-output.conf
-docker run -d --name sprio-elk -v "/tmp/logstash-input.conf:/etc/logstash/conf.d/03-logstash-input.conf:ro" -v "/tmp/logstash-output.conf:/etc/logstash/conf.d/30-output.conf:ro" -v "/tmp/elk-data:/var/lib/elasticsearch" -p 5601:5601 -p 10042:10042/udp sebp/elk:es232_l232_k450
+mkdir -p /tmp/logstash
+cp -u ./logstash-input.conf /tmp/logstash/03-logstash-input.conf
+cp -u ./logstash-output.conf /tmp/logstash/30-output.conf
+docker run -d --name sprio-elk -v "/tmp/logstash:/etc/logstash/conf.d:ro" -v "/tmp/elk-data:/var/lib/elasticsearch" -p 5601:5601 -p 10042:10042/udp sebp/elk:es232_l232_k450
